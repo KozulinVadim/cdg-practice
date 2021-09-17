c2a43ba45116608dcb09b47023b8e47c6d04551f
Vadim Kozulin <vadimwhite11@mail.ru>
2021-09-17 17:53:25 +0200

    2lab2task

--- 2lab/2lab2task.rb
+++ 2lab/2lab2task.rb
@@ -1,1 +1,17 @@
-test
+def pokemon
+  i = 0
+  puts "Сколько добавить покемонов?"
+  number = gets.to_i
+  array = []
+  while i < number
+    puts "Введите Имя покемона "
+    pokemon_name = gets
+    puts "Введите Цвет покемона "
+    pokemon_color = gets
+    hash = {"name" =>pokemon_name.chomp , "color" => pokemon_color.chomp} # хэш покемона
+    array[i] = hash # по очереди каждый хэш добавляем в массив
+    i = i+1
+  end
+  puts array
+end
+pokemon
