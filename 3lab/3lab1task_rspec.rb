ca91709d1ec2d8009378601fa75338599187032d
KozulinVadim <vadim_white11@mail.ru>
2021-12-02 14:21:26 +0300

    add 3 files

---
+++ 3lab/3lab1task_rspec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require 'C:\Users\Vadim\Desktop\myrep\cdg-practice\3lab\3lab1task.rb'
+
+RSpec.describe 'lab3task1 ' do
+  it '#index' do
+    index
+    expect(index).to be "1 q
+qq
+3q
+"
+  end
+    end
+
+
